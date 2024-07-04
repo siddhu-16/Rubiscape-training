541783959c8b33422947971d7f12d695d290f50e
siddhu-16 <siddharthjawahire@gmail.com>
2024-07-04 10:59:14 +0530

    push

--- Text-Formatter/src/app/app.module.ts
+++ Text-Formatter/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TextDisplayComponent } from './text-display/text-display.component';
+import { FormattersComponent } from './formatters/formatters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TextDisplayComponent,
+    FormattersComponent
   ],
   imports: [
     BrowserModule,
---
+++ Text-Formatter/src/app/formatters/formatters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formatters',
+  templateUrl: './formatters.component.html',
+  styleUrls: ['./formatters.component.css']
+})
+export class FormattersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
