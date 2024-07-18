96c482410fa3fd9cc594e5ff777575f294e91347
siddhu-16 <siddharthjawahire@gmail.com>
2024-07-18 11:57:43 +0530

    commit

---
+++ Text-Formatter/src/app/modules/home/remvepipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RemvepipePipe } from './remvepipe.pipe';
+
+describe('RemvepipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RemvepipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- Text-Formatter/src/app/modules/home/remvepipe.pipe.ts
+++ Text-Formatter/src/app/modules/home/remvepipe.pipe.ts
@@ -1,15 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'removeSpecialChar',
-  pure : false
+  name: 'remvepipe',
+  pure: false
 })
-export class RemoveSpecialCharPipe implements PipeTransform {
+export class RemvepipePipe implements PipeTransform {
 
   transform(value: string): string {
-    if(!value){
-      return value
-    }
     return value.replace(/[^a-zA-Z0-9 ]/g, '');
   }
 
